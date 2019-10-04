4b25f445f350e17ef2437ef880e36d66407bc595
Marcelo Rosa <marcelo.rosa.br@gmail.com>
2019-10-03 22:19:56 -0300

    oi

---
+++ ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace MultiFaceRec
+{
+   public class ConnectionString
+    {
+       public string DBConn = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=D:\Develop\DevelopC#\Face-Detection-Based-Attendance-System-master\Attendance.mdf;Integrated Security=True";
+    }
+}
